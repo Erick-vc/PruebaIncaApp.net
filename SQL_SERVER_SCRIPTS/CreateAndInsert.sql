52752a7428f5188c006b5a20052178bd7971a572
Erick Villalobos <109572912+Erick-vc@users.noreply.github.com>
2023-06-25 20:14:16 -0500

    Update CreateAndInsert.sql

--- SQL_SERVER_SCRIPTS/CreateAndInsert.sql
+++ SQL_SERVER_SCRIPTS/CreateAndInsert.sql
@@ -10,12 +10,10 @@ create table Usuario(
 	createAt datetime DEFAULT CURRENT_TIMESTAMP
 )
 
-drop table Usuario;
-
 insert into Usuario(Nombre, Apellido, Correo) values 
 ('Erick', 'Villalobos Casanatan', 'villalobose143@gmail.com'),
 ('Jacky', 'Mendoza Ramirez', 'ramirez@gmail.com'),
-('Angélica', 'Recursos Humanos', 'ange@gmail.com'),
+('AngÃ©lica', 'Recursos Humanos', 'ange@gmail.com'),
 ('Maria', 'Delgado Moreno', 'mademo@gmail.com'),
 ('Jose', 'Mateo Mendo', 'majo@gmail.com'),
 ('Prueba1', 'Test Prueba', 'test@gmail.com'),
